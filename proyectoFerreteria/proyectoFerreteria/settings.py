ecf9755d51bfd2e7a838f9469b4682e15be52579
Joaquín Francisco Osorio Valenzuela <jfosorioval@gmail.com>
2024-05-08 22:11:36 -0400

    v1.01
    Implementación de tailwind y comprobación de funcionalidad.

--- proyectoFerreteria/proyectoFerreteria/settings.py
+++ proyectoFerreteria/proyectoFerreteria/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moduloCliente',
+    'django_browser_reload',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'proyectoFerreteria.urls'
@@ -67,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+NPM_BIN_PATH = 'npm.cmd'
 
 WSGI_APPLICATION = 'proyectoFerreteria.wsgi.application'
 
@@ -122,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
