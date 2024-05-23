32ccf05868f3faf3db558eef9b708115139c010f
marcos <ma.hernandezo@duocuc.cl>
2024-05-22 23:52:42 -0400

    coneccion con base de datos

--- proyectoFerreteria/proyectoFerreteria/settings.py
+++ proyectoFerreteria/proyectoFerreteria/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'proyectoFerreteria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ferremas',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
