9936d73be2459610635a8b7d5160b40650415b05
marcos <ma.hernandezo@duocuc.cl>
2024-05-28 23:05:19 -0400

    barra de busqueda lista

--- proyectoFerreteria/moduloVendedor/views.py
+++ proyectoFerreteria/moduloVendedor/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Producto
-
+from django.db.models import Q
 # Create your views here.
 def vendedor(request):
+    busqueda = request.GET.get("buscar")
     Productos = Producto.objects.all()
+    if busqueda:
+        Productos = Producto.objects.filter(
+            Q(id_producto__icontains = busqueda) |
+            Q(nombre__icontains = busqueda) 
+        ).distinct()
     return render(request, 'inicioVendedor.html', {'Productos':Productos})
